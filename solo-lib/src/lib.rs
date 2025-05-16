405ecfb5bc86ef583e6bf517035468465119d6f1
Lance <2124757129@qq.com>
2025-05-16 23:16:36 +0800

    Version bump

--- solo-lib/src/lib.rs
+++ solo-lib/src/lib.rs
@@ -1,16 +1,17 @@
 //! # Solo Library
 //!
 //! Library for [the Solo app](https://github.com/cnlancehu/solo).
+//! 
+//! [Documentation](https://solo.lance.fun/)
 //!
-//! A lightweight tool that dynamically secures server ports.
-//!
-//! Solo automatically detects the current public IP, dynamically adjusts firewall rules through the cloud provider's API, and locks the source IP of the specified port (such as SSH/22) to the current IP.
+//! A lightweight port protection tool.
 //!
 //! ## Supported Platforms
-//! | Service Provider |         Products          |
-//! | :----: | :---------------------: |
-//! | Tencent Cloud | Cloud Server and Lighthouse |
-//! | Aliyun | Elastic Compute Service (ECS) and Simple Application Server (SAS) |
+//! 
+//! |   Provider    |                      Products                      |
+//! | :-----------: | :------------------------------------------------: |
+//! | Tencent Cloud |         Cloud Virtual Machine, Lighthouse          |
+//! |    Aliyun     | Elastic Compute Service, Simple Application Server |
 
 pub mod client;
 mod error;
